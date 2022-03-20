f4e140b54a910ac77408924192ad486fe30f9774
adlinaj <75617264+adlinaj@users.noreply.github.com>
2022-03-21 06:19:09 +0800

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'index',
+    loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)
+  },
+  {
+    path: 'court',
+    loadChildren: () => import('./court/court.module').then( m => m.CourtPageModule)
+  },
+  {
+    path: 'view-post',
+    loadChildren: () => import('./view-post/view-post.module').then( m => m.ViewPostPageModule)
+  },
+  {
+    path: 'edit-post/:id',
+    loadChildren: () => import('./edit-post/edit-post.module').then( m => m.EditPostPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ post.mode.ts
@@ -0,0 +1,7 @@
+export interface Post {
+    name: string;
+    place: string;
+    period: string;
+    date: string;
+    email: string;
+   }
